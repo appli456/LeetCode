93cef91f6378b87f9fb5671a494615ed6fe5d361
lirun <lirunzhi99@hotmail.com>
2017-01-28 02:16:35 +0800

    inital question-300

---
+++ 300/Solution.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __300_SOLUTION_H__
+#define __300_SOLUTION_H__
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+	int lengthOfLIS(vector<int>& nums) {
+
+		return 0;
+	}
+};
+#endif
---
+++ 300/main.cpp
@@ -0,0 +1,17 @@
+// 300.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "Solution.h"
+
+using std::endl;
+using std::cout;
+
+int main()
+{
+	Solution *s = new Solution();
+
+    return 0;
+}
+
