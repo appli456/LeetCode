8cefd9c975a1e2613b5f11ecdf8e3b30f9d1783e
lirun <lirunzhi99@hotmail.com>
2017-02-02 18:10:37 +0800

    300 finish

--- 300/Solution.h
+++ 300/Solution.h
@@ -5,10 +5,49 @@
 using std::vector;
 
 class Solution {
+private:
+	static int binary_search(vector<int>* contain, int target)
+	{
+		int left = 0,
+			right = contain->size() - 1;
+
+		while (left <= right)
+		{
+			int mid = (left + right) / 2;
+			if (contain->at(mid) < target)
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+		}
+
+		return left;
+	}
 public:
 	int lengthOfLIS(vector<int>& nums) {
+		unsigned size = nums.size();
+		if (size <= 1)
+		{
+			return size;
+		}
+		vector <int> result;
 
-		return 0;
+		for (auto num: nums)
+		{
+			if (result.size() == 0 || 
+				num > result.back())
+			{
+				result.push_back(num);
+			}
+			else
+			{
+				result.at(binary_search(&result, num)) = num;
+			}
+		}
+		return result.size();
 	}
 };
 #endif
--- 300/main.cpp
+++ 300/main.cpp
@@ -12,6 +12,8 @@ int main()
 {
 	Solution *s = new Solution();
 
+	cout << s->lengthOfLIS(vector<int>({ 10, 9, 2, 5, 3, 7, 101, 18 })) << endl;
+	system("pause");
     return 0;
 }
 
