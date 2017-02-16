6d256f38bf0ee6cf9aafee20c9203504f52ac0ef
lirun <lirunzhi99@hotmail.com>
2017-02-16 22:25:25 +0800

    257

---
+++ 257/Solution.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef __257__SOLUTION_H__
+#define __257__SOLUTION_H__
+#include <vector>
+#include <string>
+#include <sstream>
+#include "TreeNode.h"
+using std::vector;
+using std::string;
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	vector<string> binaryTreePaths(TreeNode* root) {
+		vector<string>path;
+		if (!root)
+		{
+			return path;
+		}
+		preorder_traveral(root, &path, std::to_string(root->val));
+		return path;
+	}
+
+	void preorder_traveral(TreeNode* node, vector<string>* const path_container, string path)
+	{
+		if (!node->left && !node->right)
+		{
+			path_container->push_back(path);
+			return;
+		}
+
+		if (node->left)
+		{
+			preorder_traveral(node->left, path_container, path + this->link + std::to_string(node->left->val));
+		}
+		if (node->right)
+		{
+			preorder_traveral(node->right, path_container, path + this->link + std::to_string(node->right->val));
+		}
+	}
+
+private:
+	const string link = "->";
+
+};
+#endif
--- 476/Solution.h
+++ 476/Solution.h
@@ -3,36 +3,42 @@
 #define __476__SOLUTION_H__
 #include <vector>
 using std::vector;
-constexpr int MAX_INT = ~(1 << 31);
+// constexpr int MAX_INT = ~(1 << 31);
 class Solution {
 public:
 	int findComplement(int num) {
-		if (num == 0 || num == 1 || num == MAX_INT)
+		unsigned int target = ~0;
+		while (num & target)
 		{
-			return 0;
+			target = target << 1;
 		}
-		int count = 0;
-		vector<unsigned int>power_of_2;
-		for (int i = 0; i < 32; ++i)
-		{
-			power_of_2.push_back(1 << i);
-		}
-		int bits = 0;
-		for (int i = 0; i < 32; ++i)
-		{
-			if (num < power_of_2[i])
-			{
-				bits = i;
-				break;
-			}
-			else if (num == power_of_2[i])
-			{
-				bits = i + 1;
-				break;
-			}
-		}
-		unsigned int sum = (this->sum(&power_of_2, bits) + num);
-		return ~sum;
+		return ~target ^ num;
+		//if (num == 0 || num == 1 || num == MAX_INT)
+		//{
+		//	return 0;
+		//}
+		//int count = 0;
+		//vector<unsigned int>power_of_2;
+		//for (int i = 0; i < 32; ++i)
+		//{
+		//	power_of_2.push_back(1 << i);
+		//}
+		//int bits = 0;
+		//for (int i = 0; i < 32; ++i)
+		//{
+		//	if (num < power_of_2[i])
+		//	{
+		//		bits = i;
+		//		break;
+		//	}
+		//	else if (num == power_of_2[i])
+		//	{
+		//		bits = i + 1;
+		//		break;
+		//	}
+		//}
+		//unsigned int sum = (this->sum(&power_of_2, bits) + num);
+		//return ~sum;
 	}
 	static unsigned int sum (vector<unsigned int>* container, int end)
 	{
