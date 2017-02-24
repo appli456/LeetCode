e5c31b66b1e62972454c00c48f21aabe91e877c9
lirun <lirunzhi99@hotmail.com>
2017-02-24 15:10:04 +0800

    145

--- 145/Solution.h
+++ 145/Solution.h
@@ -9,11 +9,34 @@
 * };
 */
 #include <vector>
+#include <stack>
 #include "../lib/TreeNode.h"
 using std::vector;
+using std::stack;
 class Solution {
 public:
 	vector<int> postorderTraversal(TreeNode* root) {
+		stack<TreeNode*>traversal_stack;
+		vector<int>postorder_traversal;
+		if (!root)
+		{
+			return postorder_traversal;
+		}
+		traversal(root->left, postorder_traversal);
+		traversal(root->right, postorder_traversal);
+		postorder_traversal.push_back(root->val);
+		return postorder_traversal;
+	}
 
+	void traversal(TreeNode* node, vector<int>& postorder_traversal)
+	{
+		if (!node)
+		{
+			return;
+		}
+
+		traversal(node->left, postorder_traversal);
+		traversal(node->right, postorder_traversal);
+		postorder_traversal.push_back(node->val);
 	}
 };
